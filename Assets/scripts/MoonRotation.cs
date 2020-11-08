96624e5f1deb57fdf24b764f08de163c6f9b7ea2
Zeb <sebastian@tekydevelop.com>
2020-11-08 07:50:39 +0200

    Project structure / Basic layout implementation

---
+++ Assets/scripts/MoonRotation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonRotation : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform rotationCenter;
+
+    [SerializeField]
+    float rotationRadius = 2f;
+
+    [SerializeField]
+    float angularSpeed = 2f;
+
+    private float posX = 0f;
+    private float posY = 0f;
+    private float angle = 0f;
+
+    void Start()
+    {
+        if (rotationCenter == null)
+        {
+           GameObject[] earth = GameObject.FindGameObjectsWithTag("earth");
+            if (earth.Length > 0)
+            {
+                rotationCenter = earth[0].transform;
+            }
+        }
+        
+    }
+
+
+
+    void Update()
+    {
+        posX = rotationCenter.position.x + Mathf.Cos(angle) * rotationRadius;
+        posY = rotationCenter.position.y + Mathf.Sin(angle) * rotationRadius/2;
+
+        transform.position = new Vector2(posX, posY);
+        angle = angle + Time.deltaTime * angularSpeed;
+
+        if (angle >= 360f)
+        {
+            angle = 0f;
+        }
+    }
+}
---
+++ Assets/scripts/SystemHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemHandler : MonoBehaviour
+{
+
+    [SerializeField]
+    public GameObject panel;
+
+    public void showBuildPanel(bool isVisible)
+    {
+        panel.SetActive(isVisible);
+    }
+
+
+}
