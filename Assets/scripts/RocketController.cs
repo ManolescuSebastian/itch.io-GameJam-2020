5a1e5ff36ad4679159c606a7a3b5b1c1ecc77f30
Zeb <sebastian@tekydevelop.com>
2020-11-09 07:55:55 +0200

    Rocket blaster particle animation / Code improvement

---
+++ Assets/scripts/RocketController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour
+{
+    [SerializeField]
+    public float speed;
+    [SerializeField]
+    public float maxSpeed = 1f;
+    [SerializeField]
+    public float smoothTime = 3f;
+    private bool shuttleStart = false;
+
+    [SerializeField]
+    private Rigidbody2D rb2d;
+    [SerializeField]
+    private ParticleSystem flameParticleSystem;
+
+
+    void Start()
+    {
+        if (rb2d != null)
+        {
+            return;
+        }
+        rb2d = GetComponent<Rigidbody2D>();
+        //flameParticleSystem = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        if (shuttleStart)
+        {
+            speed = Mathf.Lerp(speed, maxSpeed, smoothTime);
+            rb2d.transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+    }
+
+    public void isShuttleStart(bool isEngaged)
+    {
+        shuttleStart = isEngaged;
+    }
+
+    public void showFlameParticles()
+    {
+        flameParticleSystem.Play();
+    }
+}
